a352232fc88fcfe7bafaea88216bf587bf335530
Agata Dunaj <a.dunaj@yahoo.co.uk>
2021-02-18 15:48:04 +0100

    change int to string

--- kalkulator.py
+++ kalkulator.py
@@ -2,18 +2,18 @@ import sys
 import logging
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(message)s')
 print("Witaj, ten oto mini-kalkulator wykona wybrane przez Ciebie działanie na dwóch elementach.")
-dzialanie = int(print(input("Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie:\n")))
-el1 = int(print(input("Podaj pierwszy element działania")))
-el2 = int(print(input("Podaj drugi element działania")))
+dzialanie = print(input("Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie:\n"))
+el1 = print(input("Podaj pierwszy element działania: "))
+el2 = print(input("Podaj drugi element działania: "))
 def kalk(dzialanie, el1, el2):
     if dzialanie == '1':
-        return(el1 + el2)
+        print(el1 + el2)
     if dzialanie == '2':
-        return(el1 - el2)
+        print(el1 - el2)
     if dzialanie == '3':
-        return(el1 * el2)
+        print(el1 * el2)
     if dzialanie == '4':
-        return(el1 / el2)
+        print(el1 / el2)
 
 if __name__ == "__main__":
     if len(sys.argv) < 4:
@@ -22,3 +22,4 @@ if __name__ == "__main__":
     el_2 = sys.argv[3]
     dzialanie = sys.argv[1]
     kalk(dzialanie, el_1, el_2)
+print(kalk)
